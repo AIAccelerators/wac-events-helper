9b086056dd9d1f80ea549d3a58c681a2658a2dd9
Oleksandr Kovalenko2 <oleksandr_kovalenko2@epam.com>
2026-06-01 18:00:20 +0300

    chore: Bump version to 0.3.1 (patch release) per semantic versioning policy

--- GetEvents.user.js
+++ GetEvents.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GetEvents
 // @namespace    http://tampermonkey.net/
-// @version      0.4.0
+// @version      0.3.1
 // @description  Fetch and display AI events from wearecommunity.io via API
 // @author       Oleksandr_Kovalenko2
 // @connect      wearecommunity.io/api/v2
